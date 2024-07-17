1211c82e29c8c4475a5bcd310885c4d307605051
precious-appiah <appiahprecious99@gmail.co>
2024-07-17 17:25:44 +0000

    adding cors

--- portfolio_proj_backend/app.py
+++ portfolio_proj_backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 from models.auth import AuthSystem
 
 app = Flask(__name__)
+CORS(app)
 user = AuthSystem()
 
 @app.route('/signup', methods=['POST'])
